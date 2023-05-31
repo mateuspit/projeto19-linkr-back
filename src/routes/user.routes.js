aa6c4226982527908b085f62ed8c1c815c2864a9
MarkDiasBR <marcelo.dias@ufba.br>
2023-05-31 16:05:41 -0300

    fix: export name user controllers

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,7 +3,7 @@ import { schemaValidation } from '../middlewares/schemaValidation.middleware.js'
 import { signupSchema, signinSchema } from '../schemas/user.schemas.js';
 import signupConflictValidation from '../middlewares/signup.middleware.js';
 import signinValidation from '../middlewares/signin.middleware.js';
-import { signup, signin } from '../controllers/user.controller.js';
+import { signup, signin } from '../controllers/user.controllers.js';
 
 const userRouter = Router();
 
