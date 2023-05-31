1cbf499f7a71a6fb1884c1fc567cd0418f016288
MarkDiasBR <marcelo.dias@ufba.br>
2023-05-31 16:23:03 -0300

    fix: signup middleware username and email verification

--- src/middlewares/signup.middleware.js
+++ src/middlewares/signup.middleware.js
@@ -1,7 +1,7 @@
 import db from '../database/database.connection.js';
 
 export default async function signupConflictValidation(req, res, next) {
-    const { email } = req.body;
+    const { email, username } = req.body;
 
     try {
         const promise = await db.query(`
@@ -12,6 +12,16 @@ export default async function signupConflictValidation(req, res, next) {
         `, [email]);
         if (promise.rowCount !== 0) {
             return res.status(409).send('🚫 E-mail already registered!');
+        }
+
+        const promise2 = await db.query(`
+            SELECT * 
+            FROM public.users
+            WHERE username=$1
+            LIMIT 1;
+        `, [username]);
+        if (promise2.rowCount !== 0) {
+            return res.status(409).send('🚫 Username already registered!');
         }
     } catch (err) {
         res.status(500).send(`🚫 Unexpected server error!\n\n${err.message}`);
