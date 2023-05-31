ba8da5ec7858664f1fad1456c70613b7e99498d6
MarkDiasBR <marcelo.dias@ufba.br>
2023-05-31 04:37:02 -0300

    feat: database connection

--- src/database/database-connection.js
+++ src/database/database-connection.js
@@ -1,13 +1,38 @@
-import pg from "pg";
-import dotenv from "dotenv";
+import chalk from 'chalk';
+import pg from 'pg';
+import dotenv from 'dotenv';
 dotenv.config();
 
-const { Pool } = pg;
+const { name, port } = databaseNameAndPort(process.env.DATABASE_URL);
 
-const configDatabase = {
-    connectionString: process.env.DATABASE_URL,
-};
+async function databaseConnection() {
 
-if (process.env.MODE === "prod") configDatabase.ssl = true;
+    const { Pool } = pg;
 
-export const db = new Pool(configDatabase);
+    const configDatabase = {   
+        connectionString: process.env.DATABASE_URL
+    };
+
+    let db = new Pool(configDatabase);
+
+    try {
+        await db.connect();
+        console.log(chalk.black.bgGreen('\n [🐘 PostgreSQL] Database connected SUCCESSFULLY! '), '🗄️📨✨')
+        console.log(chalk.white(' Database'), chalk.underline.italic.blue(name),chalk.white('connected on PORT:'), 
+            chalk.underline.italic.blue(port));
+    } catch (err) {
+        console.log(chalk.white.bgRed('\n [🐘 PostgreSQL] DataBase connection failed! '), '🗄️📨🚫');
+        console.error(err.message);
+    }
+
+    return db;
+}
+
+const db = await databaseConnection();
+
+export default db;
+
+function databaseNameAndPort(dbString) {
+    const nameAndPortArray = dbString.split(':').slice(-1)[0].split('/').reverse();
+    return {name: nameAndPortArray[0], port: nameAndPortArray[1]};
+}
