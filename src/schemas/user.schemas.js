a0cba400218a84a079896fa97cceb531592a983f
MarkDiasBR <marcelo.dias@ufba.br>
2023-05-31 15:58:12 -0300

    fix: user signup schema

--- src/schemas/user.schemas.js
+++ src/schemas/user.schemas.js
@@ -1,13 +1,13 @@
 import joi from 'joi';
 
 export const signupSchema = joi.object({
-    fullname: joi.string().required(),
-    name: joi.string().required(),
     email: joi.string().email().required(),
+    username: joi.string().required(),
     password: joi.string().min(3).required(),
     confirmPassword: joi.valid(joi.ref('password')).required().messages({
         'any.only': "\"confirmPassword\" must match \"password\""
-    })
+    }),
+    pictureUrl: joi.string().uri()
 });
 
 export const signinSchema = joi.object({
