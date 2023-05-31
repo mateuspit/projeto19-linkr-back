0f27bf98766827ef93cada70ae306d52489b8ada
MarkDiasBR <marcelo.dias@ufba.br>
2023-05-31 04:50:52 -0300

    feat: routes index

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,5 +1,9 @@
-import { Router } from "express";
+import { Router } from 'express';
+// import authRouter from './auth.routes.js';
+import userRouter from './user.routes.js';
 
-const router = Router(); 
+const router = Router();
+router.use(userRouter);
+// router.use(authRouter);
 
 export default router;
