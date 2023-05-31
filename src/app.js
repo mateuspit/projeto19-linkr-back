9602b06ae24a6d6cccac9b7513b9d7a04db4f12d
MarkDiasBR <117422129+MarkDiasBR@users.noreply.github.com>
2023-05-31 18:44:26 -0300

    Revert "feat: signup and signin API and DB"

--- src/app.js
+++ src/app.js
@@ -1,19 +1,11 @@
-import express, { json } from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import chalk from 'chalk';
-import router from './routes/index.routes.js';
+import express from "express";
+import cors from "cors";
+import router from "./routes/index.routes.js";
 import apiPort from "./constants/apiPort.js";
-dotenv.config();
 
 const app = express();
-app.use(json());
 app.use(cors());
-
+app.use(express.json());
 app.use(router);
 
-// const port = process.env.PORT || 5000;
-app.listen(apiPort, () => {
-    console.log(chalk.bgMagenta('\n [Node.js/Express] Server connected SUCCESSFULLY! '), '🖥️⚙️✨');
-    console.log(chalk.white(' Server connected on PORT:'), chalk.blue.italic.underline(`${apiPort}`));
-});
+app.listen(apiPort, () => console.log(`API runing on port ${apiPort}`));
